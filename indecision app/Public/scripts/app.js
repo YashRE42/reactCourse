e56aff56353ad12a84d243db3c41afed2eca5d1c
Re42 <33805964+Re42@users.noreply.github.com>
2018-03-22 11:56:49 +0530

    Add more data

--- indecision app/Public/scripts/app.js
+++ indecision app/Public/scripts/app.js
@@ -3,11 +3,66 @@
 console.log("app.js is working");
 
 //jsx
+var app = {
+    title: "Who now?",
+    subtitle: "that was painful",
+    options: ["One", "Two"]
+};
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Hi dude"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        " ",
+        app.subtitle,
+        " "
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "here are your options" : "You have no options :("
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item 1"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item 2"
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Yash Rathore"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 20"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Allahabad "
+    )
 );
 
 var appRoot = document.getElementById("app");
