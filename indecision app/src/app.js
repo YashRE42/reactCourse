21f5e23485dcb92d34707b098d92483cad548884
Re42 <33805964+Re42@users.noreply.github.com>
2018-03-31 21:52:24 +0530

    add some methods into indecision

--- indecision app/src/app.js
+++ indecision app/src/app.js
@@ -27,20 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick(){
+        alert('pick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll(){
+        alert('All to be removed');
+    }
     render() {
         return (
             <div>
-                Options component here
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option)=><Option key={option} optionText={option} />)
                 }
@@ -60,10 +66,20 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e){
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+        if(option){
+            alert(option);
+        }
+    }
     render() {
         return (
             <div>
-                AddOptions component here
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" />
+                    <button>Add option </button>
+                </form>
             </div>
         );
     }
