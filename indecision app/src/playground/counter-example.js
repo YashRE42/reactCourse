5eb13e9df85b988574e0d9868233bee5ee0fcb48
Re42 <33805964+Re42@users.noreply.github.com>
2018-03-31 23:22:33 +0530

    rebuild counter-example with react components: create counter component

--- indecision app/src/playground/counter-example.js
+++ indecision app/src/playground/counter-example.js
@@ -1,32 +1,46 @@
-
-const appRoot = document.getElementById("app");
-
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={reset}>reset</button>
-            <button onClick={minusOne}>-1</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
+class Counter extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Count: </h1>
+                <button>+1</button>
+                <button>reset</button>
+                <button>-1</button>
+            </div>
+        )
+    }
 };
 
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// const appRoot = document.getElementById("app");
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={reset}>reset</button>
+//             <button onClick={minusOne}>-1</button>
+//         </div>
+//     );
+
+//     ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
