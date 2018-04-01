460b3205aa5855cf329b6639dab81b748ea11c02
Re42 <33805964+Re42@users.noreply.github.com>
2018-04-01 12:11:48 +0530

    Delete comments

--- indecision app/src/playground/counter-example.js
+++ indecision app/src/playground/counter-example.js
@@ -42,35 +42,3 @@ class Counter extends React.Component {
 };
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-// const appRoot = document.getElementById("app");
-
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// }
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// }
-
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// }
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={reset}>reset</button>
-//             <button onClick={minusOne}>-1</button>
-//         </div>
-//     );
-
-//     ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
