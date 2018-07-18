f8d0e63601543a8c1d7372f94620a014d4de5941
Re42 <33805964+Re42@users.noreply.github.com>
2018-07-18 22:08:10 +0530

    practice using defaults in playground

--- indecision app/src/playground/counter-example.js
+++ indecision app/src/playground/counter-example.js
@@ -1,3 +1,5 @@
+// count
+
 class Counter extends React.Component {
     constructor(props){
         super(props);
@@ -5,7 +7,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: 0
+            count: props.count
         };
     }
     handleAddOne(){
@@ -40,5 +42,9 @@ class Counter extends React.Component {
         )
     }
 };
+
+Counter.defaultProps = {
+    count: 0
+}
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
