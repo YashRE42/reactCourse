4882935cee23a7e5f46db7a7b9e95816cd83380d
Re42 <33805964+Re42@users.noreply.github.com>
2018-07-22 21:27:13 +0530

    add better source map to help during debugging using react dev tools

--- indecision app/webpack.config.js
+++ indecision app/webpack.config.js
@@ -15,6 +15,7 @@ module.exports ={
             exclude: /node_modules/
         }]
     },
+    devtool: "cheap-module-eval-source-map",
     devServer:{
         contentBase: path.join(__dirname, "public")
     }
