01f7f45f22270320f8d9440236ed23088890bcf7
Re42 <33805964+Re42@users.noreply.github.com>
2018-07-22 16:02:50 +0530

    try defualt export with isSenior in person.js

--- indecision app/src/app.js
+++ indecision app/src/app.js
@@ -1,15 +1,16 @@
-// import "./utils.js"
-import subtract2, {square, add} from "./utils.js"
+// // import "./utils.js"
+// import subtract2, {square, add} from "./utils.js"
 
-console.log(square(4));
+// console.log(square(4));
 
-console.log(add(100,23));
+// console.log(add(100,23));
 
-console.log(subtract2(10000,8766));
+// console.log(subtract2(10000,8766));
 
-import {isAdult, canDrink} from "./person.js"
+import isSenior,{isAdult, canDrink} from "./person.js"
 
 console.log(isAdult(1));
 console.log(canDrink(20));
+console.log(isSenior(78));
 
 console.log("app.js is running!");
