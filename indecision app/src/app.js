ec7d84812538e504ef784c51af2492ca5662c4ee
Re42 <33805964+Re42@users.noreply.github.com>
2018-07-22 17:52:17 +0530

    install and import thirdparty npm modules

--- indecision app/src/app.js
+++ indecision app/src/app.js
@@ -1,16 +1,3 @@
-// // import "./utils.js"
-// import subtract2, {square, add} from "./utils.js"
+import validator from "validator";
 
-// console.log(square(4));
-
-// console.log(add(100,23));
-
-// console.log(subtract2(10000,8766));
-
-import isSenior,{isAdult, canDrink} from "./person.js"
-
-console.log(isAdult(1));
-console.log(canDrink(20));
-console.log(isSenior(78));
-
-console.log("app.js is running!");
+console.log(validator.isEmail("nopenope@gmail.com"));
