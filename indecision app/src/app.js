6c36bc9d5424a57651a197f76f2c38704ac5339a
Re42 <33805964+Re42@users.noreply.github.com>
2018-07-22 14:49:35 +0530

    add utils import example

--- indecision app/src/app.js
+++ indecision app/src/app.js
@@ -1,4 +1,9 @@
-import "./utils.js"
+// import "./utils.js"
+import {square, add} from "./utils.js"
+
+console.log(square(4));
+
+console.log(add(100,23));
 
 import {isAdult, canDrink} from "./person.js"
 
