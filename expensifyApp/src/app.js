b4b4328ff54ceb375f731257d9d7f394d3228db5
Re42 <33805964+Re42@users.noreply.github.com>
2018-08-05 19:50:30 +0530

    test react router with two routes

--- expensifyApp/src/app.js
+++ expensifyApp/src/app.js
@@ -1,8 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import 'normalize.css/normalize.css'
 
 import './styles/styles.scss'
 
-ReactDOM.render(<p> this is the expensify app </p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        this is from ExpenseDashboardPage
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        this is from my AddExpensePage
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={ExpenseDashboardPage}/>
+        <Route path="/create" component={AddExpensePage}/>
+      </div>
+    </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, document.getElementById('app'));
