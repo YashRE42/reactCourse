83e2654713188618398760e7bdf8e04fe25795f1
Re42 <33805964+Re42@users.noreply.github.com>
2018-08-05 23:11:06 +0530

    Try react links

--- expensifyApp/src/app.js
+++ expensifyApp/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import 'normalize.css/normalize.css'
 
@@ -32,19 +32,32 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404- <Link to="/">go back</Link>
     </div>
-)
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <Link to="/">Dashboard </Link>
+        <Link to="/create">Create expense </Link>
+        <Link to="/edit">Edit expense </Link>
+        <Link to="/help">help</Link>
+    </header>
+);
 
 const routes = (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
-        <Route component={NotFoundPage}/>
-      </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
