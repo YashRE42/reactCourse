c1d0465450883a5e7804dadc91fbe5e938084205
Re42 <33805964+Re42@users.noreply.github.com>
2018-09-11 18:00:24 +0530

    create contact page and add route for it

---
+++ portfolio-website/src/components/ContactPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ContacPage = () => (
+    <div>
+        <h1>Contact Me</h1>
+        <p>you can reach me at test@gmail.com</p>
+    </div>
+);
+
+export default ContacPage;
--- portfolio-website/src/routers/AppRouter.js
+++ portfolio-website/src/routers/AppRouter.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-import HomePage from "../components/HomePage"
 import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header";
+import HomePage from "../components/HomePage";
+import ContactPage from "../components/ContactPage";
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={HomePage} />
+                <Route path="/" component={HomePage} exact={true} />
+                <Route path="/contact" component={ContactPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
