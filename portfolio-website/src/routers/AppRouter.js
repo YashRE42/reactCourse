065667d81784053daea5078c4b84ea4e5a51e551
Re42 <33805964+Re42@users.noreply.github.com>
2018-09-11 17:57:32 +0530

    Create home page and add route for it in app router

--- portfolio-website/src/routers/AppRouter.js
+++ portfolio-website/src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-
+import HomePage from "../components/HomePage"
 import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header";
 
@@ -9,6 +9,7 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
+                <Route path="/" component={HomePage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
